1f393f5c2970a0af38afbf814b649d4a8c8207f4
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-10-02 12:53:10 +0300

    Filter methods

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+let drivers= ["Stacy", "Tatianna", "Chelsea", "Manchester","Duolingo", "Duostand"];
+
+function findMatching(drivers,name){
+    return drivers.filter(driver=> driver.toLowerCase() === name.toLowerCase());
+}
+
+
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.startsWith(name))
+}
+
+let driverObj = [
+    {
+        name:"Cassidy",
+        hometown:"States"
+    }, 
+    {
+        name:"Paul",
+        hometown:"Georgia"
+    },
+    {
+        name:"Cassidy",
+        hometown:"Atlanta"
+    },
+    {
+        name:"Nafula", 
+        hometown:"Kisumu"
+    }
+]
+function matchName(driverObj, name){
+    //return each element whose name property matches the string
+    return driverObj.filter(driver=> driver.name === name )
+
+}
